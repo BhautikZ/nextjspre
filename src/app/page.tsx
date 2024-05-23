4e33397a7a9459db73bd40ea06d17a3c1ed8a290
BhautikZ <bhautikztridhyatech@gmail.com>
2024-05-23 16:22:38 +0530

    -remove project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <div className="flex flex-col  gap-20">
           <Hero />
           <Encryption />
-          <Projects />
+          {/* <Projects /> */}
         </div>
       </main>
     </>
